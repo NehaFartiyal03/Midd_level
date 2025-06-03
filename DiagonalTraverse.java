ee25d8f6f22a05c1219588c9f880fca5371e0a06
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-03 16:05:46 +0530

    Create DiagonalTraverse.java

---
+++ DiagonalTraverse.java
@@ -0,0 +1,17 @@
+class DiagonalTraverse {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        for(int i = 0; i< n; i++){
+            sum += mat[i][i];
+            sum += mat[i][n-i-1];
+        }
+        if(n % 2 == 1) {
+            sum -= mat[n/2][n/2];
+        }
+        
+        return sum;
+        
+    }
+}

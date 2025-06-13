88bd4e8108d61ae5530682a3a1f48ff4110cad25
Neha Fartiyal <1000024339@dit.edu.in>
2025-06-13 21:53:26 +0530

    Create MinimumSwapsToSequance.java

---
+++ MinimumSwapsToSequance.java
@@ -0,0 +1,26 @@
+class MinimumSwapsToSequance {
+    public int minSwap(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+
+        int[] keep = new int[n];
+        int[] swap = new int[n];
+
+        keep[0] = 0;
+        swap[0] = 1;
+
+        for(int i = 1; i < n; i++){
+            keep[i] = swap[i] = n;
+            if(nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
+                keep[i] = keep[i - 1];
+                swap[i] = swap[i-1] +1;
+            }
+            if(nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1]) {
+                keep[i] = Math.min(keep[i], swap[i-1]);
+                swap[i] = Math.min(swap[i], keep[i-1]+1);
+            }
+            
+        }
+        return Math.min(keep[n - 1], swap[n -1]);
+        
+    }
+}
